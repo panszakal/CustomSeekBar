104094752b452b28f9f548feb631b5294a9354c4
panszakal <marcin.barton@outlook.com>
2015-06-11 21:48:13 +0200

    Initial commit

--- app/src/main/java/com/szakal/customseekbar/CustomSB.java
+++ app/src/main/java/com/szakal/customseekbar/CustomSB.java
@@ -1,16 +1,19 @@
 package com.szakal.customseekbar;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.*;
 import android.graphics.drawable.GradientDrawable;
+
 import android.util.AttributeSet;
-import android.util.Log;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 
 import android.os.Handler;
-
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 
 public class CustomSB extends View {
@@ -42,6 +45,11 @@ public class CustomSB extends View {
     private int timeLabelColor;
     private int timeTextSize;
 
+    private ClipData data;
+    private DragShadowBuilder shadowBuilder;
+    int positionX, positionY;
+    private float dragX, dragY;
+    View view;
 
     public CustomSB(Context context) {
 
@@ -70,11 +78,9 @@ public class CustomSB extends View {
 
         mDrawableShape = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[] { 0xFF0000FF, 0xFF00FF00, 0xFFFF0000 });
         mDrawableShape.setShape(GradientDrawable.RECTANGLE);
-
         mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.thumb);
         mBitmapHeight = mBitmap.getHeight();
         mBitmapWidth = mBitmap.getWidth();
-
 
         mStep = 10;
         mDistance = 0;
@@ -88,18 +94,15 @@ public class CustomSB extends View {
         mTime = 0;
         isRunning = false;
 
-
-
-
-
-
+        data = ClipData.newPlainText("", "");
+        shadowBuilder = new View.DragShadowBuilder(this);
     }
 
     @Override
     protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
         super.onSizeChanged(xNew, yNew, xOld, yOld);
 
-        mShape = new Rect(mScreenWidth/10, (int)(mScreenHeight * 3/8),(int)(mScreenWidth * 0.9)  ,(int)(mScreenHeight * 5/8));
+        mShape = new Rect(mScreenWidth/10, mScreenHeight * 3/8,(int)(mScreenWidth * 0.9)  , mScreenHeight * 5/8);
         mBitmapX = mShape.left;
         mBitmapY = mShape.top - mBitmapHeight;
     }
@@ -109,24 +112,33 @@ public class CustomSB extends View {
 
         mScreenWidth = MeasureSpec.getSize(widthMeasureSpec);
         mScreenHeight = MeasureSpec.getSize(heightMeasureSpec);
-
+        view = (View)getParent();
         this.setMeasuredDimension(mScreenWidth, mScreenHeight);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     public boolean onTouchEvent(MotionEvent motionEvent) {
 
-        int positionX = (int) motionEvent.getX();
-        int positionY = (int) motionEvent.getY();
+        view.setOnDragListener(new OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+
+                onDragView(v, event);
+                return true;
+            }
+        });
+
+        positionX = (int) motionEvent.getX();
+        positionY = (int) motionEvent.getY();
         mDistance = 0;
 
         switch (motionEvent.getAction()) {
 
             case MotionEvent.ACTION_DOWN: {
 
-                if ((positionX >= mBitmapX) && (positionX <= (mBitmapX + mBitmapWidth)) && (positionY >= mBitmapY) && (positionY <= (mBitmapY + mBitmapHeight)))  {
+                handler.postDelayed(dragRunnable, 1000);
 
-                    Log.e(getRootView().toString(), "");
+                if ((positionX >= mBitmapX) && (positionX <= (mBitmapX + mBitmapWidth)) && (positionY >= mBitmapY) && (positionY <= (mBitmapY + mBitmapHeight)))  {
 
                     isMovable = true;
                     prevX = positionX;
@@ -136,12 +148,14 @@ public class CustomSB extends View {
 
             case  MotionEvent.ACTION_MOVE: {
 
+                handler.removeCallbacks(dragRunnable);
+
                 if (isMovable) {
 
                     positionX = (int)motionEvent.getX();
                     int deltaX = positionX - prevX;
 
-                    if ((deltaX != 0) && (positionX >= (mShape.left + mBitmapWidth)) && (positionX <= mShape.right) ) {
+                    if ((deltaX != 0) && (positionX >= (mShape.left + mBitmapWidth/2)) && (positionX <= mShape.right) ) {
 
                         mBitmapX = positionX - mBitmapWidth/2;
 
@@ -161,6 +175,7 @@ public class CustomSB extends View {
             case MotionEvent.ACTION_UP: {
 
                 isMovable = false;
+                handler.removeCallbacks(dragRunnable);
 
                 int leftDifference = Math.abs(mBitmapX + mBitmapWidth/2 - mShape.left);
                 int rightDifference = Math.abs(mBitmapX + mBitmapWidth/2 - mShape.right);
@@ -210,6 +225,47 @@ public class CustomSB extends View {
         return true;
     }
 
+    private void onDragView(View v, DragEvent event) {
+
+        int X = (int) event.getX();
+        int Y = (int) event.getY();
+
+        switch (event.getAction()) {
+
+            case DragEvent.ACTION_DROP: {
+
+                View _view = (View) event.getLocalState();
+                ViewGroup owner = (ViewGroup) _view.getParent();
+                owner.removeView(_view);
+
+                FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) _view.getLayoutParams();
+
+                if (X < dragX) {
+                    layoutParams.leftMargin = layoutParams.leftMargin - ((int)dragX - X);
+                    layoutParams.rightMargin = layoutParams.rightMargin - ((int)dragX - X);
+                } else {
+                    layoutParams.leftMargin = layoutParams.leftMargin + (X - (int)dragX);
+                    layoutParams.rightMargin = layoutParams.rightMargin + (X - (int)dragX);
+                }
+
+                if (Y < dragY) {
+                    layoutParams.topMargin = layoutParams.topMargin - ((int)dragY - Y);
+                    layoutParams.bottomMargin = layoutParams.bottomMargin - ((int)dragY - Y);
+                } else {
+                    layoutParams.topMargin = layoutParams.topMargin + (Y - (int)dragY);
+                    layoutParams.bottomMargin = layoutParams.bottomMargin + (Y - (int)dragY);
+                }
+
+                _view.setLayoutParams(layoutParams);
+                owner.addView(_view);
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -231,17 +287,17 @@ public class CustomSB extends View {
             mMatrix.reset();
             mPathMeasure.getPosTan(mDistance, pos, tan);
             mMatrix.postTranslate(pos[0] - mBitmapWidth, pos[1] - mBitmapHeight);
+            drawTimeText(mTime + " s.", canvas, mShape);
             canvas.drawBitmap(mBitmap, mMatrix, mPaint);
             mBitmapX = (int)pos[0] - mBitmapWidth;
             drawProgressBar(canvas);
-            drawTimeText(mTime + " s.", canvas, mShape);
             mDistance += mStep;
             mPath.reset();
             invalidate();
         } else {
+            drawTimeText(mTime + " s.", canvas, mShape);
             canvas.drawBitmap(mBitmap, mBitmapX, mBitmapY, mPaint);
             drawProgressBar(canvas);
-            drawTimeText(mTime + " s.", canvas, mShape);
         }
     }
 
@@ -311,5 +367,13 @@ public class CustomSB extends View {
         requestLayout();
     }
 
+    private Runnable dragRunnable = new Runnable() {
+        @Override
+        public void run() {
 
+            dragX = positionX;
+            dragY = positionY;
+            view.startDrag(data, shadowBuilder, view, 0);
+        }
+    };
 }
